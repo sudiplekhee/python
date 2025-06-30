a2c79ad11ccda3f4bb92f03d16df7245ed60e337
sudip chaudhary <netanddic178@gmail.com>
2025-06-30 21:45:43 +0545

    first commit

---
+++ Day1.py
@@ -0,0 +1,41 @@
+#This is my first Python program
+print("I like kfc")
+print("It's Really Good")
+
+#Strings
+first_name="Sudip"
+food="kfc"
+email="lekheesudip123@gmail.com"
+print(f"Hello{first_name}")
+print(f"My favorite food is {food}")
+print(f"My email is:{email}")
+
+#Integers
+age=21
+quantity=3
+number_of_student=20
+print(f"my age is:{age}")
+print(f"You are buying {quantity} items")
+print(f"There are {number_of_student} students")
+
+#float
+price=10.99
+gpa=3.76
+distance=5.5
+print(f"The car price is  {price} okay")
+print(f"your gpa is {gpa}")
+print(f"you ran {distance} km")
+
+#Bolean
+is_student= True 
+# print(f"Are you a student?:{is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+    
+for_sale= False
+if for_sale:
+    print("That items is for sale")
+else:
+    print("That item is not available")   
