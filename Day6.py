f1a48da82a8e3c07fe289510844b4b1f739b34fe
sudip chaudhary <netanddic178@gmail.com>
2025-07-10 22:17:46 +0545

    loop

--- Day6.py
+++ Day6.py
@@ -88,28 +88,32 @@
 
 # For loops=execute a block a fixed number of times. You can iterate over a range, String, Sequence etc.
 
-for alphabet in range(1,100):
-    print(alphabet)
-for alphabet in reversed (range(1,100)): #reversed means paxadi dheki print garcha
-    print(alphabet)
-for x in range(1,100,2): # last ma 2 cha ni 1 pachi 3 print garcha euta number chodera print garcha
-    print(x)
+# for alphabet in range(1,100):
+#     print(alphabet)
+# for alphabet in reversed (range(1,100)): #reversed means paxadi dheki print garcha
+#     print(alphabet)
+# for x in range(1,100,2): # last ma 2 cha ni 1 pachi 3 print garcha euta number chodera print garcha
+#     print(x)
 
-credit_card="1234-567-890-23"
-for i in credit_card:    #credit card ko sabai number print garcha
-    print(i)
+# credit_card="1234-567-890-23"
+# for i in credit_card:    #credit card ko sabai number print garcha
+#     print(i)
     
-for x in range(1,22):
-    if x==13:
-        # break #13 vanda mathy jadaina
-        continue #13 lai hataucha ani aru print garcha   break ani continue euta keywords ho
-    else:
-     print(x)
-     
-     
+# for x in range(1,22):
+#     if x==13:
+#         # break #13 vanda mathy jadaina
+#         continue #13 lai hataucha ani aru print garcha   break ani continue euta keywords ho
+#     else:
+#      print(x)
 import time
 my_time=int(input("Enter times:"))
 for x in range(1,my_time):
+    print(x)
+    time.sleep(1)  #3 sec pachi excute huncha
+print("Times up")
+
+my_time=int(input("Enter times:"))
+for x in range(my_time,0,-1):
     print(x)
     time.sleep(1)  #3 sec pachi excute huncha
 print("Times up")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,3 @@
-for alphabet in range(1,100):
-#     print(alphabet)
-# for alphabet in reversed (range(1,100)): #reversed means paxadi dheki print garcha
-#     print(alphabet)
-# for x in range(1,100,2): # last ma 2 cha ni 1 pachi 3 print garcha euta number chodera print garcha
-#     print(x)
-
-# credit_card="1234-567-890-23"
-# for i in credit_card:    #credit card ko sabai number print garcha
-#     print(i)
-    
-# for x in range(1,22):
-#     if x==13:
-#         # break #13 vanda mathy jadaina
-#         continue #13 lai hataucha ani aru print garcha   break ani continue euta keywords ho
-#     else:
-#      print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
