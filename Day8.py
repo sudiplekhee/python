0b5b0150b2532991bceecc0e792016cf7a35010f
sudip chaudhary <netanddic178@gmail.com>
2025-07-22 09:41:18 +0545

    left

--- Day8.py
+++ Day8.py
@@ -27,64 +27,64 @@
 #     print("You lose!")
 
 
-# import random
-# # ● ┌ ┐ │ └ ┘
-# "┌---------┐ "
-# "│         │"
-# "│         │"
-# "│         │"
-# "└---------┘"
+import random
+# ● ┌ ┐ │ └ ┘
+"┌---------┐ "
+"│         │"
+"│         │"
+"│         │"
+"└---------┘"
 
-# dice_art={
-#     1:("┌---------┐",  #These all are key values pair
-#        "│         │",
-#        "│    #    │",
-#        "│         │",
-#        "└---------┘"),
-#     2:("┌---------┐",  #These all are key values pair
-#        "│   #     │",
-#        "│         │",
-#        "│        ●│",
-#        "└---------┘"),
-#     3:("┌---------┐",  #These all are key values pair
-#        "│  ●      │",
-#        "│    ●    │",
-#        "│       ● │",
-#        "└---------┘"),
-#     4:("┌---------┐",  #These all are key values pair
-#        "│  ●    ● │",
-#        "│         │",
-#        "│  ●    ● │",
-#        "└---------┘"),
-#     5:("┌---------┐",  #These all are key values pair
-#        "│  ●    ● │",
-#        "│     ●   │",
-#        "│  ●    ● │",
-#        "└---------┘"),
-#  6:   ("┌---------┐",  #These all are key values pair
-#        "│ ●  ●  ● │",
-#        "│         │",
-#        "│ ●  ●  ● │",
-#        "└---------┘")
-# }
-# dice=[]
-# total=0
-# num_of_dice=int(input("How many dice?:"))
-# for die in range(num_of_dice):
-#  dice.append(random.randint(1, 6))
-# print(dice)
+dice_art={
+    1:("┌---------┐",  #These all are key values pair
+       "│         │",
+       "│    #    │",
+       "│         │",
+       "└---------┘"),
+    2:("┌---------┐",  #These all are key values pair
+       "│   #     │",
+       "│         │",
+       "│        ●│",
+       "└---------┘"),
+    3:("┌---------┐",  #These all are key values pair
+       "│  ●      │",
+       "│    ●    │",
+       "│       ● │",
+       "└---------┘"),
+    4:("┌---------┐",  #These all are key values pair
+       "│  ●    ● │",
+       "│         │",
+       "│  ●    ● │",
+       "└---------┘"),
+    5:("┌---------┐",  #These all are key values pair
+       "│  ●    ● │",
+       "│     ●   │",
+       "│  ●    ● │",
+       "└---------┘"),
+ 6:   ("┌---------┐",  #These all are key values pair
+       "│ ●  ●  ● │",
+       "│         │",
+       "│ ●  ●  ● │",
+       "└---------┘")
+}
+dice=[]
+total=0
+num_of_dice=int(input("How many dice?:"))
+for die in range(num_of_dice):
+ dice.append(random.randint(1, 6))
+print(dice)
 
-# for die in range(num_of_dice):
-#    for line in dice_art.get(dice[die]):
-#       print(line)
-# for line in range(5):
-#    for die in dice:
-#       print(dice_art.get(die)[line], end="")
-#    print()
+for die in range(num_of_dice):
+   for line in dice_art.get(dice[die]):
+      print(line)
+for line in range(5):
+   for die in dice:
+      print(dice_art.get(die)[line], end="")
+   print()
 
-# for die in dice:
-#    total += die
-# print(f"Total:{total}")
+for die in dice:
+   total += die
+print(f"Total:{total}")
 
 
 # #function =A block of reusable code
